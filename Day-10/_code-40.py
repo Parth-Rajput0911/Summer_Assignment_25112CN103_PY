54f82254dc80f5b5abd600f5028743ef7b8d8882
Parth-Rajput0911 <parthvikramsingh100@gmail.com>
2026-07-04 15:00:50 +0530

    Add implementation for printing a pyramid pattern of letters

---
+++ Day-10/_code-40.py
@@ -0,0 +1,12 @@
+rows = int(input("Enter rows: "))
+
+for i in range(rows):
+    print(" " * (rows - i - 1), end="")
+
+    for j in range(i + 1):
+        print(chr(65 + j), end="")
+
+    for j in range(i - 1, -1, -1):
+        print(chr(65 + j), end="")
+
+    print()
