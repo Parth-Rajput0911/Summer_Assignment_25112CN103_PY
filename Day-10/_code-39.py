748b5eca5c3f727dea0511c79b94c1fab6bc492d
Parth-Rajput0911 <parthvikramsingh100@gmail.com>
2026-07-04 15:00:38 +0530

    Add implementation for printing a pyramid pattern of numbers

---
+++ Day-10/_code-39.py
@@ -0,0 +1,12 @@
+rows = int(input("Enter rows: "))
+
+for i in range(1, rows + 1):
+    print(" " * (rows - i), end="")
+
+    for j in range(1, i + 1):
+        print(j, end="")
+
+    for j in range(i - 1, 0, -1):
+        print(j, end="")
+
+    print()
